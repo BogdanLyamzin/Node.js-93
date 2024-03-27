dbd803276e0deab3ee175771324743c395b68200
unknown <bogdan.lyamzin.d@gmail.com>
2024-03-27 21:13:54 +0200

    finish lesson 12

---
+++ lesson-12/websocket-basic-example/app.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wsServer = new WebSocketServer({ port: 5000 });
+
+const socketList = [];
+
+wsServer.on("connection", (socket) => {
+    // console.log("New frontend connected");
+    setTimeout(() => socket.send("Welcome to websocket server"), 3000);
+    socketList.forEach(item => item.send(`Now we have ${socketList.length + 1} members`));
+    socketList.push(socket);
+})
